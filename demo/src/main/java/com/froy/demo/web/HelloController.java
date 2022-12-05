0a09356e5b70353f2a21a087853ea77ddea44730
Froy Gaeta <froy.armando.huizar@ibm.com>
2022-12-05 01:40:21 -0600

    Update controller

--- demo/src/main/java/com/froy/demo/web/HelloController.java
+++ demo/src/main/java/com/froy/demo/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.froy.demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class HelloController {
-	@RequestMapping (value = "/", method = RequestMethod.GET)
+
+	@RequestMapping("/basic")
 	public String sayHello() {
-		return "Hello";
+		return "<h1>Hello, world!!!</h1>";
+
+	}
+
+	@RequestMapping("/proper")
+	public String sayProperHello() {
+		return "<h1>Hello, how are you?</h1>";
 	}
 }
