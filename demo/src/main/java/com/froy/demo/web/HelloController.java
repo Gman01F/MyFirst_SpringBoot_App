9fe77506170e8b07b8ca8bd7127e1e9b8d837716
Froy Gaeta <froy.armando.huizar@ibm.com>
2022-12-07 22:52:54 -0600

    User Controller Update

--- demo/src/main/java/com/froy/demo/web/HelloController.java
+++ demo/src/main/java/com/froy/demo/web/HelloController.java
@@ -1,6 +1,9 @@
 package com.froy.demo.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +19,26 @@ public class HelloController {
 	@RequestMapping("/proper")
 	public String sayProperHello() {
 		return "<h1>Hello, how are you?</h1>";
+	}
+	
+	@RequestMapping(value = "/user_entry")
+	public String userForm() {
+		return "<form action=\"/greeting/user_greeting\" method = \"GET\">\r\n"
+				+ "  <label for=\"fname\">firstname:</label><br>\r\n"
+				+ "  <input type=\"text\" id=\"fname\" name=\"fname\">\r\n"
+				+ "  <label for=\"lname\">lastname:</label><br>\r\n"
+				+ "  <input type=\"text\" id=\"lname\" name=\"lname\">\r\n"
+				+ "  <input type=\"submit\" value=\"Submit\">\r\n"
+				+ "</form> ";
+	}
+	
+	@RequestMapping(value = "/user_greeting", method = RequestMethod.GET)
+	public String printUserGreeting(@RequestParam String fname , @RequestParam String lname) {
+		return "Hello, " + fname + " " + lname;
+	}
+	
+	@RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
+	public String getOrder(@PathVariable String id) {
+		return "Your order is: " + id;
 	}
 }
