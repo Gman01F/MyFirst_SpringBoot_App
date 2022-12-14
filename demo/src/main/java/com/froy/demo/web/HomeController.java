ddac69f091647c8cd57270e7cd7776b5ac6d0c91
Froy Gaeta <froy.armando.huizar@ibm.com>
2022-12-13 23:59:39 -0600

    Updates to home controller

--- demo/src/main/java/com/froy/demo/web/HomeController.java
+++ demo/src/main/java/com/froy/demo/web/HomeController.java
@@ -1,9 +1,12 @@
 package com.froy.demo.web;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.froy.demo.domain.User;
 
@@ -17,7 +20,13 @@ public class HomeController {
 	}
 
 	@RequestMapping(value="/create",method=RequestMethod.POST)
-	public String processFormdata(User user) {
+	public String processFormdata(User user, RedirectAttributes attr) {
+		attr.addFlashAttribute("user", user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping("/display")
+	public String displayFormData(User user) {
 		return "result";
 	}
 }
